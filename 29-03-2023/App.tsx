4f990306e5dd2cf5c375d19e82024ec7b7e49dc1
Aariz Ansari <71026544+AarizAnsari@users.noreply.github.com>
2023-03-29 15:29:51 +0530

    Update App.tsx

--- 29-03-2023/App.tsx
+++ 29-03-2023/App.tsx
@@ -6,14 +6,14 @@
  */
 
 import React from 'react';
+
 import type {PropsWithChildren} from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
   TextInput,
   Button
